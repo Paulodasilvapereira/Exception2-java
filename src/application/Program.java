aa18b35e3b65617c0dc737102d498d1789de1c79
Paulo Silva <paulosilva9517@gmail.com>
2022-06-24 12:42:21 -0300

    exercicio

---
+++ src/application/Program.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+import exceptions.BusinessException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+
+		Account acc1 = new Account(number, holder, balance, withdrawLimit);
+
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+
+		try {
+			acc1.Withdraw(amount);
+			System.out.println("Novo saldo: " + String.format("%.2f", acc1.getBalance()));
+		} catch (BusinessException e) {
+			System.out.println(e.getMessage());
+		}
+
+		sc.close();
+	}
+
+}
